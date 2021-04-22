8665aafd7530c87a4afa843256509c68c49dfe50
kaisman75 <kaismanai1975gmail.com>
2021-04-22 23:58:13 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
-import React, { Component } from 'react'
-
-export class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>hello</h1>
-			</div>
-		)
-	}
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>To Do App</h1>
+      
+    </div>
+    <input className="form-control form-control-lg" type="text" placeholder="New Task"></input>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
